6f6381c736c80d27334548a7e9fc209342b6d424
Shiva yadav <143000205+Shivayadav72@users.noreply.github.com>
2024-09-16 23:08:41 +0530

    added javascript

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings: ["FullStack Developer, Python cyber Security, Python Web Developer"],
+    typeSpeed: 70,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+});
